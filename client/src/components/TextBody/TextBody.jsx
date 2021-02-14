5386dd78f1bde275d8c7c837c4c1881ca2cf8e81
lee-ma <leema9321@gmail.com>
2021-02-14 02:07:22 -0700

    fix text body scroll issue

--- client/src/components/TextBody/TextBody.jsx
+++ client/src/components/TextBody/TextBody.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const TextBody = ({title, htmlToRender}) => {
   return (
     <div className="text-container">
-      <h1>{title}</h1>
+      <h1 style={{marginTop: 0}}>{title}</h1>
       {htmlToRender}
     </div>
   )
