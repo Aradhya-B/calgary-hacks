0e7497511123aacf3584f24477d3533f693d0a79
lee-ma <leema9321@gmail.com>
2021-02-14 00:30:24 -0700

    remove static data

--- client/src/views/Upload.jsx
+++ client/src/views/Upload.jsx
@@ -85,33 +85,7 @@ class Upload extends Component {
 
       // Request made to the backend api
       // Send formData object
-      // await axios.post("http://localhost:5000/upload", formData)
-      const res = await new Promise((resolve, reject) => resolve({data: [{
-        "keywords":[
-           "degradation",
-           "persistence",
-           "message",
-           "Stability"
-        ],
-        "summary":[
-           {
-              "summary_text":"It could be that in certain cells, the message is protected so that it hangs around longer. And, in other cells, perhaps, it's unprotected and it's degraded very rapidly. If it's persistent for a long time, it can make a lot of copies of protein."
-           }
-        ],
-        "text":"Stability means the persistence of the message, the degradation of the message. It could be that in certain cells, the message is protected so that it hangs around longer. And, in other cells, perhaps, it's unprotected and it's degraded very rapidly. If it's degraded very rapidly, it doesn't get a chance to make a protein or maybe it doesn't get to make too many copies of the protein. If it's persistent for a long time, it can make a lot of copies of protein. "
-     },
-     {
-        "keywords":[
-           "mRNA",
-           "translation"
-        ],
-        "summary":[
-           {
-              "summary_text":"The ability to regulate translation in a number of different ways is important. There are exciting new genes called micro RNA's that can interfere with translatability. organisms can tweak up or down how actively a particular message is being translated. And then, of course, there's post-translational control."
-           }
-        ],
-        "text":"All of those things can and do occur. Then, of course, there is the regulation at the level of translation. Translation, if I give you an mRNA, is it automatically going to be translated? Maybe the cell has a way to sequester the RNA to ramp it up in some way so that it doesn't get to the ribosome under some conditions, and under other conditions it does get to the ribosome, or some ways to block, in other manners than just sequestering it, but to physically block whether or not this message gets translated. Well, it turns out that there's a tremendous amount of that. It's, again, not the most common, but we're learning, particularly over the last couple of years, that regulation of the translation of an mRNA is important. There are, although I won't talk about them at length, an exciting new set of genes called micro RNA's, teeny little RNAs that encode 21-22 base pair segments that are able to pair with a messenger RNA and interfere in some ways partially with its translatability. And so, by the number and the kinds of little micro RNAs that are there, organisms can tweak up or down how actively a particular message is being translated. So, the ability to regulate translation in a number of different ways is important. And then, of course, there's post-translational control. "
-     }]}));
+      const res =await axios.post("http://localhost:5000/upload", formData)
       const keywords = [...new Set(res.data.reduce((accum, arr) => accum.concat(arr.keywords), []))];
       const summary = res.data.reduce((accum, arr) => {accum.push(arr.summary[0].summary_text); return accum;}, []).join('\n');
       const originalText = res.data.reduce((accum, arr) => {accum.push(arr.text); return accum;}, []).join('\n');
